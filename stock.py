b833112c6d23c8ba7e4424a701f3e0344d6686db
Melique <daley.melique@gmail.com>
2018-06-14 00:05:47 -0400

    Added basic error handeling to each method in Stock. Fixed inconsistencies

--- stock.py
+++ stock.py
@@ -8,6 +8,7 @@ TODO:
 -add important attributes
 -price earning ratio
 -destory function
+-fix the init error
 '''
 import pandas
 import info
@@ -48,18 +49,14 @@ class Stock:
             self.balance_sheet = simple[1]
             self.cash_flow_sheet = simple[2]
 
-        #self.gross_profit = self.income_sheet.loc["Gross profit", self.balance_sheet.columns[-1]]
-
-    #liquidity
     def current_ratio(self):
         """Returns the current ratio of a stock.
 
            Use: measures ability to pay current liabilites with current assets."""
 
-        current_assets =0.0
+        current_assets = 0.0
         current_lib = 0.0
 
-        #"Total current assets" in self.balance_sheet.index.values
         try:
             current_assets = self.balance_sheet.loc["Total current assets",self.balance_sheet.columns[-1]]
             current_lib = self.balance_sheet.loc["Total current liabilities", self.balance_sheet.columns[-1]]
@@ -74,45 +71,49 @@ class Stock:
 
            Use: Indicates percentage of assest financed with debt."""
 
-        tot_assets =0.0
-        tot_lib =0.0
-        #"Total assets" in self.balance_sheet.index.values
+        tot_assets = 0.0
+        tot_lib = 0.0
 
         try:
-            tot_assets= self.balance_sheet.loc["Total assets", self.balance_sheet.columns[-1]]
+            tot_assets = self.balance_sheet.loc["Total assets", self.balance_sheet.columns[-1]]
             tot_lib = self.balance_sheet.loc["Total liabilities", self.balance_sheet.columns[-1]]
-        except Expection as e:
-            print(e)
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            debt_ratio = tot_assets/tot_lib
+            return debt_ratio
 
-        debt_ratio = tot_assets/tot_lib
-        return debt_ratio
-
-    #acid test
     def quick_ratio(self):
         """Returns quick(acid-test) ratio of a stock.
 
            Use: Shows the ability to pay all current liabilites if the come
                 due immediately. """
 
-        cash=self.balance_sheet.loc["Cash and cash equivalents", self.balance_sheet.columns[-1]]
-        short_term_invest = self.balance_sheet.loc["Short-term investments", self.balance_sheet.columns[-1]]
-        current_receivables = self.balance_sheet.loc["Receivables", self.balance_sheet.columns[-1]]
-        current_lib = self.balance_sheet.loc["Total current liabilities", self.balance_sheet.columns[-1]]
+        try:
+            cash = self.balance_sheet.loc["Cash and cash equivalents", self.balance_sheet.columns[-1]]
+            short_term_invest = self.balance_sheet.loc["Short-term investments", self.balance_sheet.columns[-1]]
+            current_receivables = self.balance_sheet.loc["Receivables", self.balance_sheet.columns[-1]]
+            current_lib = self.balance_sheet.loc["Total current liabilities", self.balance_sheet.columns[-1]]
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = (cash + short_term_invest + current_receivables)/current_lib
+            return ratio
 
-        qratio = (cash + short_term_invest + current_receivables)/current_lib
-        return qratio
-
-    #interest coverage ratio
     def time_interest_earned_ratio(self):
         """Returns the time interest earned or interest converage ratio of a stock.
 
            Use: Measues the number of times operating income can cover interest expense."""
 
-        operating_income = self.income_sheet.loc["Operating income", self.income_sheet.columns[-2]] #want the latest annula year
-        interest_expense = self.income_sheet.loc["Interest Expense", self.income_sheet.columns[-2]]
-
-        ratio = operating_income/interest_expense
-        return ratio
+        try:
+            #want the latest year
+            operating_income = self.income_sheet.loc["Operating income", self.income_sheet.columns[-2]]
+            interest_expense = self.income_sheet.loc["Interest Expense", self.income_sheet.columns[-2]]
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = operating_income/interest_expense
+            return ratio
 
     #problem, gross margin
     def gross_profit_percentage(self):
@@ -120,36 +121,43 @@ class Stock:
 
            Use: The percentage if a profit makes before operating cost is subtracted."""
 
-        gross_profit = self.income_sheet.loc["Gross profit", self.income_sheet.columns[-2]]
-        revenue = self.income_sheet.loc["Revenue", self.income_sheet.columns[-2]]
+        try:
+            gross_profit = self.income_sheet.loc["Gross profit", self.income_sheet.columns[-2]]
+            revenue = self.income_sheet.loc["Revenue", self.income_sheet.columns[-2]]
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = (gross_profit/revenue)*100
+            return ratio
 
-        ratio = (gross_profit/revenue)*100
-        return ratio
-
-    #operating proft margin, return on sales
     def operating_income_percentage(self):
         """Returns the operating income percentage of a stock.
 
            Use: Shows the percentage of profit earned from each dollar in the company's
                 core business, after operating costs have been subtracted."""
 
-        operating_income = self.income_sheet.loc["Operating income", self.income_sheet.columns[-2]]
-        revenue = self.income_sheet.loc["Revenue", self.income_sheet.columns[-2]]
+        try:
+            operating_income = self.income_sheet.loc["Operating income", self.income_sheet.columns[-2]]
+            revenue = self.income_sheet.loc["Revenue", self.income_sheet.columns[-2]]
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = (operating_income/revenue)*100
+            return ratio
 
-        ratio = (operating_income/revenue)*100
-        return ratio
-
-    #net profit margin as well
     def return_on_net_sales(self):
-        """Retuns the return of net sales of a stock.
+        """Returns the return of net sales of a stock.
 
            Use: Shows the percentage of each sales dollar earned as net income."""
 
-        net_income = self.income_sheet.loc["Net income", self.income_sheet.columns[-2]]
-        revenue = self.income_sheet.loc["Revenue", self.income_sheet.columns[-2]]
-
-        ratio = net_income/revenue
-        return ratio
+         try:
+             net_income = self.income_sheet.loc["Net income", self.income_sheet.columns[-2]]
+             revenue = self.income_sheet.loc["Revenue", self.income_sheet.columns[-2]]
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = net_income/revenue
+            return ratio
 
     def leverage_ratio(self):
         """Returns the leverage ratio or equity multiplier of a stock.
@@ -157,29 +165,30 @@ class Stock:
            Use: How much capital comes the the form of debt or assesses the ability
                 of a company to meets its financial obligations."""
 
-        average_total = (self.balance_sheet.loc["Total assets", self.balance_sheet.columns[-1]]+
-                         self.balance_sheet.loc["Total assets", self.balance_sheet.columns[-2]])/2
-        average_equity = (self.balance_sheet.loc["Additional paid-in capital",self.balance_sheet.columns[-1]]+
-                          self.balance_sheet.loc["Additional paid-in capital",self.balance_sheet.columns[-2]])/2
-
-        ratio = average_total/average_equity
-        return ratio
-
+        try:
+            average_total = (self.balance_sheet.loc["Total assets", self.balance_sheet.columns[-1]]+
+                             self.balance_sheet.loc["Total assets", self.balance_sheet.columns[-2]])/2
+            average_equity = (self.balance_sheet.loc["Additional paid-in capital",self.balance_sheet.columns[-1]]+
+                              self.balance_sheet.loc["Additional paid-in capital",self.balance_sheet.columns[-2]])/2
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = average_total/average_equity
+            return ratio
 
     def roe(self):
         """Returns the return on equity of a stock.
 
            Use: Measues how much income is earned for every dolllar invested by
                 the company's shareholders."""
-
-        net_income = self.income_sheet.loc["Net income", self.income_sheet.columns[-2]]
-        shareholder_equity = self.balance_sheet.loc["Total stockholders' equity", self.balance_sheet.columns[-1]]
-
-        ratio = net_income/shareholder_equity
-        return ratio
-
-    #def eps(self):
-        #net_income = self.income_sheet.loc["Net income", self.income_sheet.columns[-2]]
+        try:
+            net_income = self.income_sheet.loc["Net income", self.income_sheet.columns[-2]]
+            shareholder_equity = self.balance_sheet.loc["Total stockholders' equity", self.balance_sheet.columns[-1]]
+        except KeyError:
+            return("idk ask Rachel")
+        else:
+            ratio = net_income/shareholder_equity
+            return ratio
 
     def print_ratios(self):
         """Outputs the all the above ratios of a stock."""
@@ -192,17 +201,5 @@ class Stock:
                        "Leverage Ratio: ": self.leverage_ratio(), "Return on equity: ":self.roe()}
 
         for key,value in name_value.items():
-            print(key,value)
-
-        '''
-        methods = dir(self)
-        start = methods.index("current_ratio")
-        slice = methods[start:]
-        key_words = ["income_sheet", "name", "print_ratios"]
-        ratios_name = list(filter(lambda x: x not in key_words, slice))
-
-        for ratio in ratios_name:
-            print(ratio +": " + eval("self."+str(ratio)+"()"))
-        '''
-
-    #def __del__(self):
+            if value != "idk ask Rachel":
+                print(key,value)
