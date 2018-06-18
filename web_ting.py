2612e6b84b686989ca0087170606328fb6331c40
Melique <daley.melique@gmail.com>
2018-06-17 22:00:53 -0400

    hard code version working

--- web_ting.py
+++ web_ting.py
@@ -1,44 +1,51 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
-import re
 import pandas as pd
+import re
 
-my_url = "https://www.nasdaq.com/symbol/amzn/financials?query=ratios"
+income = "https://www.nasdaq.com/symbol/amzn/financials?query=income-statement"
 
-# opening up connection, grabbing the percentage
-uClinet = uReq(my_url)
-page_html = uClinet.read()
+#opening and reading the web page
+uClinet = uReq(income)
+income_html = uClinet.read()
 uClinet.close()
 
-# html parsing
-soup = soup(page_html, "html.parser")
+#convert to a soup object
+income_soup = soup(income_html, "html.parser")
 
-#its a list of 1 element
-table_data_raw = soup.findAll("div", {"class":"genTable"})[0]
+#finding the data table
+table_data = income_soup.findAll("div", {"class":"genTable"})[0]
 
-#name of sheet. Needs fixing
-name = table_data_raw.h3.text.strip()
+#name of the sheet. Needs fixing
+name_raw = table_data.h3.text.strip()
 
+#finding the dates columns. Will used for the DF header
 dates = []
-dole = table_data_raw.findAll("th")
-for ting in dole:
-    y = re.search(r'(\d+/\d+/\d+)', ting.string)
-    #if not null
-    if y:
-        dates.append(y.group(0))
+table_headers = table_data.findAll("th")
+for header in table_headers:
+    #finding the right date format
+    temp = re.search(r'(\d+/\d+/\d+)', header.string)
+    if temp:
+        dates.append(temp.group(0))
+#finds the content in each row of table_data
+table_rows = table_data.findAll("tr")
+content = []
+for row in table_rows:
+    #each new row should have <th>. Already dealt with the first item
+    if "<th" in str(row)[1:]:
+        #name of the row
+        temp = [row.th.string]
+        #find all the data for that row
+        data_raw = row.findAll("td")
+        for item in data_raw:
+            #looking for money
+            data_pure = re.search(r'\(?\$\d*,?\d*,?\d*\)?', str(item))
+            if data_pure:
+                temp.append(data_pure.group(0))
+        content.append(temp)
 
-rows = table_data_raw.findAll("tr")
+#not needed
+content.remove(['Period Ending:'])
 
-rachel = []
-#dont the first element
-for dog in rows[1:]:
-    #are we in a new row
-    if "<th" in str(dog):
-        #name
-        helen = [x.th.string]
-        kraya = x.findAll("td")
-        for will in chris:
-            tee = re.search(r'\(?\$\d*,?\d*,?\d*\)?', str(will))
-            if tee:
-                helen.append(tee.group(0))
-        rachel.append(helen)
+print(dates)
+print(content)
